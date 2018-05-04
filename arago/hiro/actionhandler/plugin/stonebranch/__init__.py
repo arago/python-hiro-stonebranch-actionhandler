8a8248c353268498441ae87a4ad3b6fe74f23bab
Johannes Harth <jharth@arago.co>
2018-05-04 11:50:56 +0200

    resolve package issues w namespace packages; mixing both fails miserably

--- arago/hiro/actionhandler/plugin/stonebranch/__init__.py
+++ arago/hiro/actionhandler/plugin/stonebranch/__init__.py
@@ -1,10 +1,7 @@
+from arago.hiro.actionhandler.plugin.stonebranch.RestClient import RestClient
+from arago.hiro.actionhandler.plugin.stonebranch.Task import Task
+from arago.hiro.actionhandler.plugin.stonebranch.TaskInstanceState import TaskInstanceState
+from arago.hiro.actionhandler.plugin.stonebranch.TaskInstance import TaskInstance
 from arago.hiro.actionhandler.plugin.stonebranch.StonebranchAction import StonebranchAction
 from arago.hiro.actionhandler.plugin.stonebranch.StonebranchInstance import StonebranchInstance
 from arago.hiro.actionhandler.plugin.stonebranch.StonebranchRestClient import StonebranchRestClient
-from arago.hiro.actionhandler.plugin.stonebranch.Task import Task
-from arago.hiro.actionhandler.plugin.stonebranch.TaskInstance import TaskInstance
-from arago.hiro.actionhandler.plugin.stonebranch.TaskInstanceState import TaskInstanceState
-
-
-class RestClient:
-    pass
