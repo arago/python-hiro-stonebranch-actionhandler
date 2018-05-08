8f207f27dbf7f4ed8a2ea5512e65a8e1eb261860
Johannes Harth <jharth@arago.co>
2018-05-08 13:33:25 +0200

    decl namespace pkgs; dependencies and their locations; decl entry point

--- setup.py
+++ setup.py
@@ -22,10 +22,26 @@ distutils.core.setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
     ],
-    packages=[
+    entry_points={
+        'console_scripts': [
+            'stonebranch-actionhandler = arago.hiro.actionhandler.plugin.stonebranch.StonebranchActionHandlerDaemon:main',
+        ],
+    },
+    namespace_packages=[
+        'arago',
+        'arago.hiro',
+        'arago.hiro.actionhandler',
+        'arago.hiro.actionhandler.plugin',
         'arago.hiro.actionhandler.plugin.stonebranch',
+        'arago.hiro.actionhandler.plugin.stonebranch.action',
     ],
     install_requires=[
         'arago-common-base',
+        'arago-pyactionhandler',
+        'requests',
+    ],
+    dependency_links=[
+        'git+https://github.com/arago/python-arago-common.git@96c1618fc8ab861951930f768d02cb25e2adf9dd#egg=arago-common-base',
+        'git+https://github.com/arago/python-arago-pyactionhandler.git@25bed7c011f86615d97ec366c806c5269089aedd#egg=arago-pyactionhandler',
     ],
 )
